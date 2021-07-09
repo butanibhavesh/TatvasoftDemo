a2074ed0b9c15a40ae96fec9131a43f30b31a845
Bhavesh <butani.bhavesh@gmail.com>
2021-07-09 17:56:44 +0530

    final commit

--- app/src/main/java/com/tatvasoftdemo/MainActivity.kt
+++ app/src/main/java/com/tatvasoftdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -41,7 +42,7 @@ class MainActivity : AppCompatActivity() {
 
             var isValid = false
             var gridSize = 2
-            for (i in 2..5) {
+            for (i in 2..intValue) {
                 if (i * i == intValue) {
                     isValid = true
                     gridSize = i
@@ -78,19 +79,37 @@ class MainActivity : AppCompatActivity() {
     private fun randomChangeColor(totalItems: Int, list: List<GridModel>) {
         // val ex : IntArray = intArrayOf()
         var ex = ArrayList<Int>()
-        for (i in 0..list.size-1) {
-            if(list[i].selectedColor == "b") {
+        for (i in 0..list.size - 1) {
+            if (list[i].selectedColor == "b") {
                 ex.add(i)
             }
         }
 
+        if (ex.size == list.size) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Win")
+            builder.setMessage("You won the game!")
+            builder.setPositiveButton("OK") { dialog, which ->
+                dialog.dismiss()
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+            //Toast.makeText(this@MainActivity, "You won the Game!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         var rnd = Random()
-        var newRedValue = getRandomWithExclusion(rnd,0,(totalItems), ex)
+        var newRedValue = getRandomWithExclusion(rnd, 0, (totalItems), ex)
         list[newRedValue].selectedColor = "r"
         (rvGrid.adapter as GridAdapter).notifyItemChanged(newRedValue)
     }
 
-    private fun getRandomWithExclusion(rnd: Random, start: Int, end: Int, exclude: ArrayList<Int>): Int {
+    private fun getRandomWithExclusion(
+        rnd: Random,
+        start: Int,
+        end: Int,
+        exclude: ArrayList<Int>
+    ): Int {
         var random = start + rnd.nextInt(end - start + 1 - exclude.size)
         for (ex in exclude) {
             if (random < ex) {
